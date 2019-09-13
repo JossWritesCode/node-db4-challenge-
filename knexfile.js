fb1d4596df6cbb1e429984d2635660b7241b0f35
Joscelyn <JoscelynRose@gmail.com>
2019-09-12 22:38:32 -0400

    set up migration

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,12 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/db.sqlite3'
+      filename: './data/db.db3'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      }
     },
     migrations: {
       directory: './data/migrations'
